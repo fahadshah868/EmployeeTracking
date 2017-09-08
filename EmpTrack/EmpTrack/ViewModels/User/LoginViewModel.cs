1941ecbe6f799d2b0b6d457fe29b8cf08c5e0aa6
fahadshah868 <fahadshah868@gmail.com>
2017-09-08 13:04:22 +0500

    update branch

---
+++ EmpTrack/EmpTrack/ViewModels/User/LoginViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EmpTrack.ViewModels.User
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    App.Current.MainPage = new Views.Menu.MainPage();
+                });
+            }
+        }
+    }
+}
--- EmpTrack/EmpTrack/Views/Login/LoginPage.xaml.cs
+++ EmpTrack/EmpTrack/Views/Login/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmpTrack.ViewModels.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +13,12 @@ namespace EmpTrack.Views.Login
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        LoginViewModel loginViewModel;
         public LoginPage()
         {
             InitializeComponent();
-        }
-        private async void Login_Clicked(object sender,EventArgs args)
-        {
-            //await DirectoryAuthentication.SearchByAlias();
-            App.Current.MainPage = new Views.Menu.MainPage();
+            loginViewModel = new LoginViewModel();
+            BindingContext = loginViewModel;
         }
     }
 }
